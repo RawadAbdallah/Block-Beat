a29a0e70a397f0fa112ef269c03f9d34351548d2
RawadAbdallah <rawad0ne@gmail.com>
2023-11-12 15:15:56 +0200

    Setting up event Listeners

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,43 @@ const greenBlockSound = new Audio('./assets/audio/green.mp3')
 const orangeBlockSound =  new Audio('./assets/audio/orange.mp3')
 const pinkBlockSound = new Audio('./assets/audio/pink.mp3')
 const gameOverSound = document.getElementById('game-over-sound')
+
+let gameStarted = false;
+let isShowingSeq = false;
+const gameSeq = [];
+const playerSeq = [];
+let levelCounter = 1;
+
+//Adding click events for the blocks
+const addClickEventListener = (block, number, blockSound) => {
+    block.addEventListener("click", () => {
+        showBlock(block, blockSound)
+    });
+  };
+  
+  addClickEventListener(blueBlock, 1, blueBlockSound);
+  addClickEventListener(greenBlock, 2, greenBlockSound);
+  addClickEventListener(pinkBlock, 3, pinkBlockSound);
+  addClickEventListener(orangeBlock, 4, orangeBlockSound);
+  
+//To show a specific block
+const showBlock = (block, blockSound) => {
+    blockSound.play();
+    block.classList.toggle("lighten");
+    setTimeout(() => {
+      block.classList.toggle("lighten");
+    }, 100);
+};
+
+// Mute button
+muteButton.addEventListener("click", () => {
+    if (!gameMusic.paused) {
+      gameMusic.pause();
+      document.getElementById("muted-icon").classList.toggle("hidden");
+      document.getElementById("unmuted-icon").classList.toggle("hidden");
+    } else {
+      gameMusic.play();
+      document.getElementById("muted-icon").classList.toggle("hidden");
+      document.getElementById("unmuted-icon").classList.toggle("hidden");
+    }
+  });
