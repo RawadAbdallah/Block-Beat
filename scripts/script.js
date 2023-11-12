cf3f9faa2b4a09a7f39bb77b07d630592ee34412
RawadAbdallah <rawad0ne@gmail.com>
2023-11-12 14:51:22 +0200

    Adding dom elements and audio files

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+//importing elements from the DOM
+const blueBlock = document.getElementById("blue-block");
+const greenBlock = document.getElementById("green-block");
+const pinkBlock = document.getElementById("pink-block");
+const orangeBlock = document.getElementById("orange-block");
+const startGameText = document.getElementById("start-game");
+const levelCounterText = document.getElementById("level-counter");
+const gameOverScreen = document.getElementById('game-over-screen')
+const gameOverScreenScore = document.getElementById('game-over-screen-score')
+const muteButton = document.getElementById('mute-button');
+const successOverlay = document.getElementById('success-overlay')
+const playAgainBtn = document.getElementById('play-again-btn')
+
+//Audio Files
+const gameMusic = new Audio('./assets/audio/gamemusic.mp3');
+const blueBlockSound = new Audio('./assets/audio/blue.mp3')
+const greenBlockSound = new Audio('./assets/audio/green.mp3')
+const orangeBlockSound =  new Audio('./assets/audio/orange.mp3')
+const pinkBlockSound = new Audio('./assets/audio/pink.mp3')
+const gameOverSound = document.getElementById('game-over-sound')
